a39f223dddcd8c97ae41e9047d43c11175a416a9
ShinyHobo <shinyhobo1@gmail.com>
2020-10-25 18:01:31 -0500

    gui: Setting up info.json file data.

---
+++ bg3-mod-packer/bg3-mod-packer/Models/Meta.cs
@@ -0,0 +1,13 @@
+﻿namespace bg3_mod_packer.Models
+{
+    public class Meta
+    {
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public string Folder { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public string UUID { get; set; }
+        public string MD5 { get; set; }
+    }
+}
--- bg3-mod-packer/bg3-mod-packer/ViewModels/DragAndDropBox.cs
+++ bg3-mod-packer/bg3-mod-packer/ViewModels/DragAndDropBox.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Windows;
+using System.Xml;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace bg3_mod_packer.ViewModels
 {
     public class FolderDragDropHelper
     {
         /// <summary>
-        /// 
+        /// Process the file/folder drop.
         /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
+        /// <param name="data">Drop data</param>
+        /// <returns>Success</returns>
         public static bool ProcessDrop(IDataObject data)
         {
             if (data.GetDataPresent(DataFormats.FileDrop))
@@ -23,15 +25,50 @@ namespace bg3_mod_packer.ViewModels
                         // Only accept directory
                         if (Directory.Exists(fullPath))
                         {
-                            var metadata = fullPath + "/Mods/meta.lsx";
+                            var metaList = new List<string>();
+                            foreach (string mod in Directory.GetDirectories(fullPath + "/Mods"))
+                            {
+                                foreach (string file in Directory.GetFiles(mod))
+                                {
+                                    if(Path.GetFileName(file).Equals("meta.lsx"))
+                                    {
+                                        metaList.Add(file);
+                                    }
+                                }
+                            }
 
-                            if (!File.Exists(metadata))
+                            if(metaList.Count == 0)
                             {
                                 // meta.lsx not found, discontinue
                                 return false;
                             }
 
+                            var mods = new List<Models.Meta>();
 
+                            foreach(string meta in metaList)
+                            {
+                                // generate .pak files
+
+
+                                // generate info.json section
+                                XmlDocument doc = new XmlDocument();
+                                doc.Load(meta);
+
+                                var metadata = new Models.Meta
+                                {
+                                    Author = doc.SelectSingleNode("//attribute[@id='Author']").Attributes["value"].InnerText,
+                                    Name = doc.SelectSingleNode("//attribute[@id='Name']").Attributes["value"].InnerText,
+                                    Description = doc.SelectSingleNode("//attribute[@id='Description']").Attributes["value"].InnerText,
+                                    Version = doc.SelectSingleNode("//attribute[@id='Version']").Attributes["value"].InnerText,
+                                    Folder = doc.SelectSingleNode("//attribute[@id='Folder']").Attributes["value"].InnerText,
+                                    UUID = doc.SelectSingleNode("//attribute[@id='UUID']").Attributes["value"].InnerText,
+                                    MD5 = ""
+                                };
+
+                                mods.Add(metadata);
+                            }
+
+                            var json = $"{{\"Mods\":{JsonConvert.SerializeObject(mods)} }}";
                         }
                         else
                         {
