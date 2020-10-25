bb4bbdc7b4e294b647e858d2cec9fcb8c6670ea1
ShinyHobo <shinyhobo1@gmail.com>
2020-10-25 16:50:57 -0500

    gui: Added simple drag and drop box. gui: Added folder d&d helper class along with processor method.

--- bg3-mod-packer/bg3-mod-packer/Views/UserControl1.xaml.cs
+++ bg3-mod-packer/bg3-mod-packer/Views/UserControl1.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace bg3_mod_packer
+namespace bg3_mod_packer.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserControl1 : UserControl
     {
-        public MainWindow()
+        public UserControl1()
         {
             InitializeComponent();
         }
