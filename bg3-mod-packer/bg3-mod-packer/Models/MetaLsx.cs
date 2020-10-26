41d5c0be377c1119a7d2a49d87fe3a0a2aa95b0b
ShinyHobo <shinyhobo1@gmail.com>
2020-10-25 20:08:20 -0500

    gui: Finished method for processing mod folder. gui: Added text and filler color for d&d box.

--- bg3-mod-packer/bg3-mod-packer/Models/MetaLsx.cs
+++ bg3-mod-packer/bg3-mod-packer/Models/MetaLsx.cs
@@ -1,6 +1,9 @@
-﻿namespace bg3_mod_packer.Models
+﻿/// <summary>
+/// The structure of the needed data from meta.lsx
+/// </summary>
+namespace bg3_mod_packer.Models
 {
-    public class Meta
+    public class MetaLsx
     {
         public string Author { get; set; }
         public string Name { get; set; }
@@ -8,6 +11,5 @@
         public string Version { get; set; }
         public string Description { get; set; }
         public string UUID { get; set; }
-        public string MD5 { get; set; }
     }
 }
