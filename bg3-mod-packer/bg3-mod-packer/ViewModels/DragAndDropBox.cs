dd758e206473b2d6b9eb093de8c860fdf32fd507
ShinyHobo <shinyhobo1@gmail.com>
2020-10-25 20:08:20 -0500

    gui: Finished method for processing mod folder. gui: Added text and filler color for d&d box.

--- bg3-mod-packer/bg3-mod-packer/ViewModels/DragAndDropBox.cs
+++ bg3-mod-packer/bg3-mod-packer/ViewModels/DragAndDropBox.cs
@@ -3,6 +3,10 @@ using System.Windows;
 using System.Xml;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System;
+using System.IO.Compression;
+using bg3_mod_packer.Models;
 
 namespace bg3_mod_packer.ViewModels
 {
@@ -43,32 +47,79 @@ namespace bg3_mod_packer.ViewModels
                                 return false;
                             }
 
-                            var mods = new List<Models.Meta>();
+                            var mods = new List<MetaLsx>();
+                            var tempFolder = Path.GetTempPath() + "BG3ModPacker";
+                            Directory.CreateDirectory(tempFolder);
 
-                            foreach(string meta in metaList)
+                            var dirName = new DirectoryInfo(fullPath).Name;
+                            var destination = tempFolder + $"\\{dirName}.pak";
+                            var pathToDivine = string.Empty;
+                            var divine = @"/c " + pathToDivine + $" -g \"bg3\" --action \"create-package\" --source \"{fullPath}\" --destination \"{destination}\" -l \"all\"";
+
+                            // generate .pak files
+                            var process = new System.Diagnostics.Process();
+                            var startInfo = new System.Diagnostics.ProcessStartInfo
                             {
-                                // generate .pak files
+                                FileName = "cmd.exe",
+                                Arguments = divine
+                            };
+                            process.StartInfo = startInfo;
+                            process.Start();
+                            process.WaitForExit();
 
+                            var info = new InfoJson();
 
+                            // calculate md5 hash of .pak
+                            using (var md5 = MD5.Create())
+                            {
+                                using (var stream = File.OpenRead(destination))
+                                {
+                                    var hash = md5.ComputeHash(stream);
+                                    info.MD5 = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                                }
+                            }
+
+                            foreach (string meta in metaList)
+                            {
                                 // generate info.json section
                                 XmlDocument doc = new XmlDocument();
                                 doc.Load(meta);
 
-                                var metadata = new Models.Meta
+                                var metadata = new MetaLsx
                                 {
                                     Author = doc.SelectSingleNode("//attribute[@id='Author']").Attributes["value"].InnerText,
                                     Name = doc.SelectSingleNode("//attribute[@id='Name']").Attributes["value"].InnerText,
                                     Description = doc.SelectSingleNode("//attribute[@id='Description']").Attributes["value"].InnerText,
                                     Version = doc.SelectSingleNode("//attribute[@id='Version']").Attributes["value"].InnerText,
                                     Folder = doc.SelectSingleNode("//attribute[@id='Folder']").Attributes["value"].InnerText,
-                                    UUID = doc.SelectSingleNode("//attribute[@id='UUID']").Attributes["value"].InnerText,
-                                    MD5 = ""
+                                    UUID = doc.SelectSingleNode("//attribute[@id='UUID']").Attributes["value"].InnerText
                                 };
 
                                 mods.Add(metadata);
                             }
 
-                            var json = $"{{\"Mods\":{JsonConvert.SerializeObject(mods)} }}";
+                            info.Mods = mods;
+                            var json = JsonConvert.SerializeObject(info);
+                            File.WriteAllText(tempFolder + @"\info.json", json);
+
+                            // save zip next to folder that was dropped
+                            var parentDir = Directory.GetParent(fullPath);
+                            var zip = $"{parentDir.ToString()}\\{mods[0].Name}.zip";
+                            if(File.Exists(zip))
+                            {
+                                File.Delete(zip);
+                            }
+                            ZipFile.CreateFromDirectory(tempFolder, zip);
+
+                            // cleanup temp folder
+                            DirectoryInfo di = new DirectoryInfo(tempFolder);
+
+                            foreach (FileInfo file in di.GetFiles())
+                            {
+                                file.Delete();
+                            }
+
+                            return true;
                         }
                         else
                         {
