762c8f97c891b2717ca93443bb3932d12b31f5a9
ShinyHobo <shinyhobo1@gmail.com>
2020-10-27 17:45:43 -0500

    gui: Added main window model.

---
+++ bg3-mod-packer/bg3-mod-packer/Models/MainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace bg3_mod_packer.Models
+{
+    public class MainWindow : INotifyPropertyChanged
+    {
+        private string _consoleOutput;
+        public string ConsoleOutput {
+            get { return _consoleOutput; }
+            set {
+                _consoleOutput = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ConsoleOutput"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
