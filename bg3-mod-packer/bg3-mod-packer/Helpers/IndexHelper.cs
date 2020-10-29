2c5a394fedd047d0aaa96ca3e4ef80ba50bdd57e
ShinyHobo <shinyhobo1@gmail.com>
2020-10-28 22:32:13 -0500

    Downgraded 4.7.1 to 4.6.1 for use with IndexEngine. Added index helper.

---
+++ bg3-mod-packer/bg3-mod-packer/Helpers/IndexHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Indexer;
+
+namespace bg3_mod_packer.Helpers
+{
+    class IndexHelper
+    {
+    }
+}
