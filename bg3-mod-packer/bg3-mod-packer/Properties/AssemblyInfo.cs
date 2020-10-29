87718c2beedd4a32195de88d2a91707c9a1ae2ad
ShinyHobo <shinyhobo1@gmail.com>
2020-10-28 21:40:50 -0500

    Added copyright. Updated some strings.

--- bg3-mod-packer/bg3-mod-packer/Properties/AssemblyInfo.cs
+++ bg3-mod-packer/bg3-mod-packer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("bg3-mod-packer")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © ShinyHobo 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
