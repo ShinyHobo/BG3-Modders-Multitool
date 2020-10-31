d8b1136feccf667fda74687907b930799e0da60f
ShinyHobo <shinyhobo1@gmail.com>
2020-10-31 04:34:11 -0500

    Added ability to press enter to search. Added ability to click path to open file in default program.

--- bg3-mod-packer/bg3-mod-packer/Views/IndexingWindow.xaml.cs
+++ bg3-mod-packer/bg3-mod-packer/Views/IndexingWindow.xaml.cs
@@ -3,8 +3,10 @@
     using bg3_mod_packer.Helpers;
     using bg3_mod_packer.Models;
     using System.Collections.ObjectModel;
-    using System.Threading.Tasks;
+    using System.IO;
     using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
 
     /// <summary>
     /// Interaction logic for IndexingWindow.xaml
@@ -25,17 +27,34 @@
             await ((SearchResults)DataContext).IndexHelper.Index(fileList);
         }
 
-        private void SearchFiles_Click(object sender, RoutedEventArgs e)
+        private async void SearchFiles_Click(object sender, RoutedEventArgs e)
         {
             if(!string.IsNullOrEmpty(search.Text))
             {
                 var results = new ObservableCollection<SearchResult>();
-                foreach (string result in ((SearchResults)DataContext).IndexHelper.SearchFiles(search.Text))
+                foreach (string result in await ((SearchResults)DataContext).IndexHelper.SearchFiles(search.Text))
                 {
-                    results.Add(new SearchResult { Path = result.Replace(@"\\", @"\") });
+                    results.Add(new SearchResult { Path = result.Replace(@"\\", @"\").Replace($"{Directory.GetCurrentDirectory()}\\UnpackedData\\",string.Empty) });
                 }
                 ((SearchResults)DataContext).Results = results;
             }
+        }
+
+        private void Search_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SearchFiles_Click(sender, e);
+            }
+        }
+
+        private void Path_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var path = ((TextBlock)((Button)sender).Content).Text;
+            System.Diagnostics.Process fileopener = new System.Diagnostics.Process();
+            fileopener.StartInfo.FileName = "explorer";
+            fileopener.StartInfo.Arguments = $"{Directory.GetCurrentDirectory()}\\UnpackedData\\{path}";
+            fileopener.Start();
         }
     }
 }
