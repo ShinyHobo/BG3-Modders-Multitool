b9e43bae2e685679a451ef86e04eb13cc437870f
ShinyHobo <shinyhobo1@gmail.com>
2020-10-30 23:29:19 -0500

    select-pak: Added popup for selecting .paks to unpack.

---
+++ bg3-mod-packer/bg3-mod-packer/Models/PakSelection.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// The .pak selection window model.
+/// </summary>
+namespace bg3_mod_packer.Models
+{
+    using System.Collections.ObjectModel;
+    public class PakSelection
+    {
+        public ObservableCollection<CheckBox> PakList { get; set; }
+    }
+}
