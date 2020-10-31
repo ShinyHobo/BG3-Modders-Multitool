ca2ed97b268ea47f127ee50e519b0adc03ff5260
ShinyHobo <shinyhobo1@gmail.com>
2020-10-31 03:46:26 -0500

    Added popout window for searching and running index.

---
+++ bg3-mod-packer/bg3-mod-packer/Models/SearchResults.cs
@@ -0,0 +1,54 @@
+﻿namespace bg3_mod_packer.Models
+{
+    using bg3_mod_packer.Helpers;
+    using bg3_mod_packer.ViewModels;
+    using System.Collections.ObjectModel;
+
+    public class SearchResults : BaseViewModel
+    {
+        private int _resultTotal;
+
+        public int IndexFileTotal {
+            get { return _resultTotal; }
+            set {
+                _resultTotal = value;
+                OnNotifyPropertyChanged();
+            }
+        }
+
+        private int _resultCount;
+
+        public int IndexFileCount {
+            get { return _resultCount; }
+            set {
+                _resultCount = value;
+                OnNotifyPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<SearchResult> _results;
+
+        public ObservableCollection<SearchResult> Results {
+            get { return _results; }
+            set {
+                _results = value;
+                OnNotifyPropertyChanged();
+            }
+        }
+
+        public IndexHelper IndexHelper = new IndexHelper();
+    }
+
+    public class SearchResult : BaseViewModel
+    {
+        private string _path;
+
+        public string Path {
+            get { return _path; }
+            set {
+                _path = value;
+                OnNotifyPropertyChanged();
+            }
+        }
+    }
+}
---
+++ bg3-mod-packer/bg3-mod-packer/Views/IndexingWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace bg3_mod_packer.Views
+{
+    using bg3_mod_packer.Helpers;
+    using bg3_mod_packer.Models;
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for IndexingWindow.xaml
+    /// </summary>
+    public partial class IndexingWindow : Window
+    {
+        public IndexingWindow()
+        {
+            InitializeComponent();
+            DataContext = new SearchResults();
+            ((SearchResults)DataContext).IndexHelper.DataContext = (SearchResults)DataContext;
+        }
+
+        private async void IndexFiles_Click(object sender, RoutedEventArgs e)
+        {
+            var fileList = IndexHelper.DirectorySearch("UnpackedData");
+            var fileExtensions = IndexHelper.GetFileExtensions(fileList);
+            await ((SearchResults)DataContext).IndexHelper.Index(fileList);
+        }
+
+        private void SearchFiles_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(search.Text))
+            {
+                var results = new ObservableCollection<SearchResult>();
+                foreach (string result in ((SearchResults)DataContext).IndexHelper.SearchFiles(search.Text))
+                {
+                    results.Add(new SearchResult { Path = result.Replace(@"\\", @"\") });
+                }
+                ((SearchResults)DataContext).Results = results;
+            }
+        }
+    }
+}
