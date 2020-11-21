36513f2bf06d68d9f246668c31e52fd51eff1f27
ShinyHobo <shinyhobo1@gmail.com>
2020-11-21 02:08:10 -0600

    Finished addin the status data model.

---
+++ bg3-modders-multitool/bg3-modders-multitool/Enums/ValueLists/StatusGroupFlags.cs
@@ -0,0 +1,31 @@
+﻿/// <summary>
+/// The status group flags.
+/// </summary>
+namespace bg3_modders_multitool.Enums.ValueLists
+{
+    public enum StatusGroupFlags
+    {
+        SG_None,
+        SG_Condition,
+        SG_Blinded,
+        SG_Charmed,
+        SG_Cursed,
+        SG_Disease,
+        SG_Exhausted,
+        SG_Frightened,
+        SG_Incapacitated,
+        SG_Invisible,
+        SG_Poisoned,
+        SG_Prone,
+        SG_Restrained,
+        SG_Stunned,
+        SG_Unconscious,
+        SG_Surface,
+        SG_Polymorph,
+        SG_Paralyzed,
+        SG_Light,
+        SG_Disguise,
+        SG_Possessed,
+        SG_Petrified
+    }
+}
