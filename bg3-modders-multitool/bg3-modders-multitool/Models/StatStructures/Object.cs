5dd05e93362b1a5fa1b66c5e302093bbb5e065bc
ShinyHobo <shinyhobo1@gmail.com>
2020-11-20 23:15:28 -0600

    Completed the object model.

--- bg3-modders-multitool/bg3-modders-multitool/Models/StatStructures/Object.cs
+++ bg3-modders-multitool/bg3-modders-multitool/Models/StatStructures/Object.cs
@@ -1,51 +1,57 @@
-﻿
+﻿/// <summary>
+/// The object.
+/// </summary>
 namespace bg3_modders_multitool.Models.StatStructures
 {
+    using bg3_modders_multitool.Enums.ValueLists;
+    using System;
+    using System.Collections.Generic;
+
     class Object
     {
-        //modifier "RootTemplate","FixedString"
-        //modifier "Level","ConstantInt"
-        //modifier "ValueLevel","ConstantInt"
-        //modifier "ValueUUID","Guid"
-        //modifier "ValueScale","ConstantFloat"
-        //modifier "ValueRounding","ConstantInt"
-        //modifier "ValueOverride","ConstantInt"
-        //modifier "Rarity","Rarity"
-        //modifier "Weight","ConstantInt"
-        //modifier "ComboCategory","FixedString"
-        //modifier "Requirements","Requirements"
-        //modifier "Vitality","ConstantInt"
-        //modifier "Armor","ConstantInt"
-        //modifier "Flags","AttributeFlags"
-        //modifier "DefaultBoosts","FixedString"
-        //modifier "PersonalStatusImmunities","StatusIDs"
-        //modifier "InventoryTab","InventoryTabs"
-        //modifier "AddToBottomBar","YesNo"
-        //modifier "IgnoredByAI","YesNo"
-        //modifier "ObjectCategory","FixedString"
-        //modifier "MinAmount","ConstantInt"
-        //modifier "MaxAmount","ConstantInt"
-        //modifier "Priority","ConstantInt"
-        //modifier "Unique","ConstantInt"
-        //modifier "MinLevel","ConstantInt"
-        //modifier "MaxLevel","ConstantInt"
-        //modifier "UseCosts","FixedString"
-        //modifier "ItemUseType","ItemUseTypes"
-        //modifier "BludgeoningResistance","ResistanceFlags"
-        //modifier "SlashingResistance","ResistanceFlags"
-        //modifier "PiercingResistance","ResistanceFlags"
-        //modifier "AcidResistance","ResistanceFlags"
-        //modifier "ColdResistance","ResistanceFlags"
-        //modifier "FireResistance","ResistanceFlags"
-        //modifier "ForceResistance","ResistanceFlags"
-        //modifier "LightningResistance","ResistanceFlags"
-        //modifier "NecroticResistance","ResistanceFlags"
-        //modifier "PoisonResistance","ResistanceFlags"
-        //modifier "PsychicResistance","ResistanceFlags"
-        //modifier "RadiantResistance","ResistanceFlags"
-        //modifier "ThunderResistance","ResistanceFlags"
-        //modifier "UseConditions","Conditions"
-        //modifier "FallingHitEffect","FixedString"
-        //modifier "FallingLandEffect","FixedString"
+        public string RootTemplate { get; set; }
+        public int Level { get; set; }
+        public int ValueLevel { get; set; }
+        public Guid ValueUUID { get; set; }
+        public float ValueScale { get; set; }
+        public int ValueRounding { get; set; }
+        public int ValueOverride { get; set; }
+        public Rarity Rarity { get; set; }
+        public int Weight { get; set; }
+        public string ComboCategory { get; set; }
+        public string Requirements { get; set; } // Requirements
+        public int Vitality { get; set; }
+        public int Armor { get; set; }
+        public List<AttributeFlag> Flags { get; set; }
+        public string DefaultBoosts { get; set; }
+        public string PersonalStatusImmunities { get; set; } // StatusIDs
+        public InventoryTabs InventoryTab { get; set; }
+        public bool AddToBottomBar { get; set; }
+        public bool IgnoredByAI { get; set; }
+        public string ObjectCategory { get; set; }
+        public int MinAmount { get; set; }
+        public int MaxAmount { get; set; }
+        public int Priority { get; set; }
+        public int Unique { get; set; }
+        public int MinLevel { get; set; }
+        public int MaxLevel { get; set; }
+        public string UseCosts { get; set; }
+        public ItemUseTypes ItemUseType { get; set; }
+        public List<ResistanceFlag> BludgeoningResistance { get; set; }
+        public List<ResistanceFlag> SlashingResistance { get; set; }
+        public List<ResistanceFlag> PiercingResistance { get; set; }
+        public List<ResistanceFlag> AcidResistance { get; set; }
+        public List<ResistanceFlag> ColdResistance { get; set; }
+        public List<ResistanceFlag> FireResistance { get; set; }
+        public List<ResistanceFlag> ForceResistance { get; set; }
+        public List<ResistanceFlag> LightningResistance { get; set; }
+        public List<ResistanceFlag> NecroticResistance { get; set; }
+        public List<ResistanceFlag> PoisonResistance { get; set; }
+        public List<ResistanceFlag> PsychicResistance { get; set; }
+        public List<ResistanceFlag> RadiantResistance { get; set; }
+        public List<ResistanceFlag> ThunderResistance { get; set; }
+        public string UseConditions { get; set; } // Conditions
+        public string FallingHitEffect { get; set; }
+        public string FallingLandEffect { get; set; }
     }
 }
