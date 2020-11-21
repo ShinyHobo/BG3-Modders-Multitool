94563f899e708636240e2233203703e31dc79da3
ShinyHobo <shinyhobo1@gmail.com>
2020-11-21 00:50:59 -0600

    Finished adding the character class.

--- bg3-modders-multitool/bg3-modders-multitool/Models/StatStructures/Character.cs
+++ bg3-modders-multitool/bg3-modders-multitool/Models/StatStructures/Character.cs
@@ -1,55 +1,61 @@
-﻿
+﻿/// <summary>
+/// The character class.
+/// </summary>
 namespace bg3_modders_multitool.Models.StatStructures
 {
-    class Character
+    using bg3_modders_multitool.Enums.ValueLists;
+    using System;
+    using System.Collections.Generic;
+
+    public class Character
     {
-        //modifier "Level","ConstantInt"
-        //modifier "Strength","ConstantInt"
-        //modifier "Dexterity","ConstantInt"
-        //modifier "Constitution","ConstantInt"
-        //modifier "Intelligence","ConstantInt"
-        //modifier "Wisdom","ConstantInt"
-        //modifier "Charisma","ConstantInt"
-        //modifier "Armor","ConstantInt"
-        //modifier "Vitality","ConstantInt"
-        //modifier "XPReward","Guid"
-        //modifier "Sight","ConstantInt"
-        //modifier "Hearing","ConstantInt"
-        //modifier "FOV","ConstantInt"
-        //modifier "Weight","ConstantInt"
-        //modifier "StepsType","StepsType"
-        //modifier "ExtraProperties","FixedString"
-        //modifier "Flags","AttributeFlags"
-        //modifier "DefaultBoosts","FixedString"
-        //modifier "PersonalStatusImmunities","StatusIDs"
-        //modifier "PathInfluence","FixedString"
-        //modifier "Progression Type","Progression Type"
-        //modifier "ProficiencyBonus","ConstantInt"
-        //modifier "SpellCastingAbility","Ability"
-        //modifier "UnarmedAttackAbility","Ability"
-        //modifier "ActionResources","FixedString"
-        //modifier "Class","FixedString"
-        //modifier "Passives","FixedString"
-        //modifier "BludgeoningResistance","ResistanceFlags"
-        //modifier "SlashingResistance","ResistanceFlags"
-        //modifier "PiercingResistance","ResistanceFlags"
-        //modifier "AcidResistance","ResistanceFlags"
-        //modifier "ColdResistance","ResistanceFlags"
-        //modifier "FireResistance","ResistanceFlags"
-        //modifier "ForceResistance","ResistanceFlags"
-        //modifier "LightningResistance","ResistanceFlags"
-        //modifier "NecroticResistance","ResistanceFlags"
-        //modifier "PoisonResistance","ResistanceFlags"
-        //modifier "PsychicResistance","ResistanceFlags"
-        //modifier "RadiantResistance","ResistanceFlags"
-        //modifier "ThunderResistance","ResistanceFlags"
-        //modifier "Initiative","ConstantInt"
-        //modifier "Progressions","FixedString"
-        //modifier "MinimumDetectionRange","FixedString"
-        //modifier "DarkvisionRange","FixedString"
-        //modifier "FallingHitEffect","FixedString"
-        //modifier "FallingLandEffect","FixedString"
-        //modifier "ArmorType","ArmorType"
-        //modifier "Proficiency Group","ProficiencyGroupFlags"
+        public int Level { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+        public int Armor { get; set; }
+        public int Vitality { get; set; }
+        public Guid XPReward { get; set; }
+        public int Sight { get; set; }
+        public int Hearing { get; set; }
+        public int FOV { get; set; }
+        public int Weight { get; set; }
+        public StepsType StepsType { get; set; }
+        public string ExtraProperties { get; set; }
+        public List<AttributeFlag> Flags { get; set; }
+        public string DefaultBoosts { get; set; }
+        public string PersonalStatusImmunities { get; set; } // StatusIDs
+        public string PathInfluence { get; set; }
+        public ProgressionType ProgressionType { get; set; }
+        public int ProficiencyBonus { get; set; }
+        public Ability SpellCastingAbility { get; set; }
+        public Ability UnarmedAttackAbility { get; set; }
+        public string ActionResources { get; set; }
+        public string Class { get; set; }
+        public string Passives { get; set; }
+        public List<ResistanceFlag> BludgeoningResistance { get; set; }
+        public List<ResistanceFlag> SlashingResistance { get; set; }
+        public List<ResistanceFlag> PiercingResistance { get; set; }
+        public List<ResistanceFlag> AcidResistance { get; set; }
+        public List<ResistanceFlag> ColdResistance { get; set; }
+        public List<ResistanceFlag> FireResistance { get; set; }
+        public List<ResistanceFlag> ForceResistance { get; set; }
+        public List<ResistanceFlag> LightningResistance { get; set; }
+        public List<ResistanceFlag> NecroticResistance { get; set; }
+        public List<ResistanceFlag> PoisonResistance { get; set; }
+        public List<ResistanceFlag> PsychicResistance { get; set; }
+        public List<ResistanceFlag> RadiantResistance { get; set; }
+        public List<ResistanceFlag> ThunderResistance { get; set; }
+        public int Initiative { get; set; }
+        public string Progressions { get; set; }
+        public string MinimumDetectionRange { get; set; }
+        public string DarkvisionsRange { get; set; }
+        public string FallingHitEffect { get; set; }
+        public string FallingLandEffect { get; set; }
+        public ArmorType ArmorType { get; set; }
+        public ProficiencyGroupFlags Proficiencygroup { get; set; }
     }
 }
