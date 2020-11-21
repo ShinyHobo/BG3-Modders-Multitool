5fe2d466a5efa1bbbedfb6dfc194fe0b2b0d101a
ShinyHobo <shinyhobo1@gmail.com>
2020-11-21 00:24:32 -0600

    Finished the armor model.

---
+++ bg3-modders-multitool/bg3-modders-multitool/Enums/ValueLists/ProficiencyGroupFlags.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// The proficiency group flags
+/// </summary>
+namespace bg3_modders_multitool.Enums.ValueLists
+{
+    public enum ProficiencyGroupFlags
+    {
+        None,
+        Battleaxes,
+        Clubs,
+        Daggers,
+        Darts,
+        Glaives,
+        Greatswords,
+        HandCrossbows,
+        Handaxes,
+        HeavyArmor,
+        HeavyCrossbows,
+        Javelins,
+        LightArmor,
+        LightCrossbows,
+        LightHammers,
+        Longbows,
+        Longswords,
+        Maces,
+        MartialWeapons,
+        MediumArmor,
+        Quarterstaffs,
+        Rapiers,
+        Scimitars,
+        Shields,
+        Shortbows,
+        Shortswords,
+        Sickles,
+        SimpleWeapons,
+        Slings,
+        Spears,
+        Warhammers
+    }
+}
