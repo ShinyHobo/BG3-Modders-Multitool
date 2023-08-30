8745c1e03c97e5e2bb45b7806e6fa23b0ea08f85
ShinyHobo <shinyhobo1@gmail.com>
2023-08-30 11:12:25 -0500

    Added extra file extension exclusions

--- bg3-modders-multitool/bg3-modders-multitool/Services/IndexHelper.cs
+++ bg3-modders-multitool/bg3-modders-multitool/Services/IndexHelper.cs
@@ -30,7 +30,7 @@ namespace bg3_modders_multitool.Services
         // audio: .wem
         // video: .bk2
         // shaders: .bshd, .shd
-        private static readonly string[] extensionsToExclude = { ".png", ".dds", ".DDS", ".ttf", ".gr2", ".GR2", ".gtp", ".wem", ".bk2", ".ffxanim", ".tga", ".bshd", ".shd", ".jpg" };
+        private static readonly string[] extensionsToExclude = { ".bin",".png", ".dds", ".DDS", ".ttf", ".gr2", ".GR2", ".fbx", ".dae", ".gtp", ".wem", ".bk2", ".ffxanim", ".tga", ".bshd", ".shd", ".jpg",".gts",".data",".patch" };
         private static readonly string[] imageExtensions = { ".png", ".dds", ".DDS", ".tga", ".jpg" };
         public static readonly string[] BinaryExtensions = { ".lsf", ".bin", ".loca", ".data", ".patch" };
         private static readonly string luceneIndex = "lucene/index";
