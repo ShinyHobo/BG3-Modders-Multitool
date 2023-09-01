85a2e9614b067360d49b15a7f9112c393eda5961
ShinyHobo <shinyhobo1@gmail.com>
2023-09-01 13:59:57 -0500

    Added logic for closing, replacing, and re-opening the exe Cleaning update files when done Getting most recent build instead of matched build

--- bg3-modders-multitool/bg3-modders-multitool/Services/AutoUpdaterService.cs
+++ bg3-modders-multitool/bg3-modders-multitool/Services/AutoUpdaterService.cs
@@ -4,6 +4,7 @@
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
     using System;
+    using System.Diagnostics;
     using System.IO.Compression;
     using System.Linq;
     using System.Net;
@@ -39,7 +40,7 @@
             HttpClient.DefaultRequestHeaders.UserAgent.TryParseAdd("BG3-Modders-Multitool");//Set the User Agent
 
             AutoResetEvent = new AutoResetEvent(true);
-            Timer = new Timer(CheckForVersionUpdate, AutoResetEvent, 0, ((int)TimeSpan.FromSeconds(120).TotalMilliseconds));
+            Timer = new Timer(CheckForVersionUpdate, AutoResetEvent, 0, ((int)TimeSpan.FromMinutes(30).TotalMilliseconds));
         }
 
         /// <summary>
@@ -66,7 +67,7 @@
                         {
                             // release available
                             var newestTag = newestRelease["tag_name"].ToString().Remove(0, 1); // remove v
-                            var assets = matchedVersion["assets"];
+                            var assets = newestRelease["assets"];
                             if(assets != null)
                             {
                                 DownloadNewVersion(assets);
@@ -106,7 +107,7 @@
                     }
                     if(File.Exists(Path.Combine(updateDirectory, $"{exeName}.exe")))
                     {
-                        // TODO - spin up process to close and replace currently running exe
+                       ReplaceApplicationWithNewVersion();
                     } 
                     else
                     {
@@ -114,6 +115,26 @@
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Closes the current application
+        /// Starts a new background process
+        /// Replaces the application with the new one
+        /// Starts the application
+        /// </summary>
+        private void ReplaceApplicationWithNewVersion()
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke(() => {
+                System.Windows.Application.Current.Shutdown();
+                var exeName = "bg3-modders-multitool.exe";
+                var updatePath = $"{DragAndDropHelper.TempFolder}\\Update";
+                var updateExe = Path.Combine(updatePath, exeName);
+                var currentExe = Path.Combine(Directory.GetCurrentDirectory(), exeName);
+                var process = new Process();
+                process.StartInfo = new ProcessStartInfo("cmd.exe", $"/c replace {updateExe} {Directory.GetCurrentDirectory()} & rmdir {updatePath} /s /q & del {DragAndDropHelper.TempFolder}\\update.zip & start \"\" \"{currentExe}\"");
+                process.Start();
+            });
         }
     }
 }
